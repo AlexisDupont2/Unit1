9d86af95035744f81f133925a28afb5b10f79bcc
AlexisDupont2 <816522@mystma.org>
2020-09-14 20:20:05 +0000

    movies

---
+++ unit1/src/app/App.java
@@ -0,0 +1,26 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello java user");
+        System.out.println("this is my first line");
+        System.out.println("sharknato");
+        System.out.println("sharknato2");
+        System.out.println("sharknato3");
+        System.out.println("sharknato4");
+        System.out.println("sharknato5");
+        System.out.println("bee movie");
+        System.out.println("spirit");
+        System.out.println("killer bean");
+        System.out.println("star was");
+        System.out.println("sonic movie");
+
+
+
+
+
+
+
+        
+    }
+}
