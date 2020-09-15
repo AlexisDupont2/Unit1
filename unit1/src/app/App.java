6c9b20bada99289bf83058c8035f45e9c08be5ca
AlexisDupont2 <816522@mystma.org>
2020-09-15 20:17:25 +0000

    data types

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,6 +6,7 @@ public class App {
         char firstLetterOfName = 'A';
         byte howManyCookiesInACan = 50;
         
+        
         System.out.println("how many cookies ina jar?");
         System.out.println(howManyCookiesInACan);
         System.out.println("first letter of my name is:");
