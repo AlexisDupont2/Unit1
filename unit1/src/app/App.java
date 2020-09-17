d7e45cd1b982c98569158a5412fbc9f459c1043c
AlexisDupont2 <816522@mystma.org>
2020-09-17 16:52:54 +0000

    madlibs

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,16 +2,23 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesInAlbertville = false;
-        char firstLetterOfName = 'A';
-        byte howManyCookiesInACan = 50;
-        
-        
-        System.out.println("how many cookies ina jar?");
-        System.out.println(howManyCookiesInACan);
-        System.out.println("first letter of my name is:");
-        System.out.println(firstLetterOfName);
-        System.out.println("Do I live in Albertville?");
-        System.out.println(livesInAlbertville);
+    
+       String person1 = " Bob";
+       String person2 = " Patt";
+       String verb1 = " run.";
+       String verb2 = " spit";
+       String verb3 = " order";
+       String food = " chicken";
+       String dayOfWeek = " wednesday";
+       String place1 = " planet fitness";
+       String place2 = " Wendys";
+       String adjective1 = " expired";
+       String adjective2 = " smelly";
+
+       System.out.println(person1 + " is a programmer who likes to" + verb1 );
+       System.out.println(" Every " + dayOfWeek + " they make an" + adjective1 + food + " for breakfest and" + verb2 + " with" + person2 + ".");
+       System.out.println(" Sometimes, they go to the" + adjective2 + place2 + " with" + person2 + " so that they can" + verb3 + " together.");
+
+
     }
 }
