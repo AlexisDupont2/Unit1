16e276f2fa041787c7e0b624d4aeaa20ab732f3a
AlexisDupont2 <816522@mystma.org>
2020-09-21 20:15:35 +0000

    madlib input

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,24 +1,25 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    
-       String person1 = " Bob";
-       String person2 = " Patt";
-       String verb1 = " run.";
-       String verb2 = " spit";
-       String verb3 = " order";
-       String food = " chicken";
-       String dayOfWeek = " wednesday";
-       String place1 = " planet fitness";
-       String place2 = " Wendys";
-       String adjective1 = " expired";
-       String adjective2 = " smelly";
 
-       System.out.println(person1 + " is a programmer who likes to" + verb1 );
-       System.out.println(" Every " + dayOfWeek + " they make an" + adjective1 + food + " for breakfest and" + verb2 + " at" + place1 + " with" + person2 + ".");
-       System.out.println(" Sometimes, they go to the" + adjective2 + place2 + " with" + person2 + " so that they can" + verb3 + " together.");
+       Scanner input = new Scanner(System.in);
 
+       System.out.println("Please enter a place.");
+       String place = input.nextLine();
+       System.out.println("Ben regularly visits the " + place + " every tuesday.");
+       System.out.println("Please enter an adjective ");
+       String adjective = input.nextLine();
+       System.out.println("Please enter a food");
+       String food = input.nextLine();
+       System.out.println("While at " + place + " Ben eats " + adjective + " " + food);
+       System.out.println("Please enter a verb");
+       String verb= input.nextLine();
+       System.out.println("He normally " + verb + " after eating");
+       System.out.println("Please enter an adjective");
+       System.out.println("he likes " + adjective + " friends");
+       
 
     }
 }
