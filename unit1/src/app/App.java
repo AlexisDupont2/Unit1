59b6573766d5987cd19f5333be6bc5150f8c6b04
AlexisDupont2 <816522@mystma.org>
2020-09-22 20:12:47 +0000

    purple number

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,31 +1,24 @@
 package app;
 import java.util.Scanner;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-
-       Scanner input = new Scanner(System.in);
-
-       System.out.println("Please enter a place.");
-       String place = input.nextLine();
-       System.out.println("Please enter an adjective ");
-       String adjective1 = input.nextLine();
-       System.out.println("Please enter a food (plural)");
-       String food = input.nextLine();
-       System.out.println("Please enter a verb");
-       String verb1= input.nextLine();
-       System.out.println("Please enter an adjective");
-       String adjective2 = input.nextLine();
-       System.out.println("Please enter a name");
-       String name = input.nextLine();
-       System.out.println("Please enter a job");
-       String job = input.nextLine();
-       System.out.println("he likes " + adjective2 + " friends");
-       System.out.println("Ben regularly visits the " + place + " every tuesday.");
-       System.out.println("While at " + place + " Ben eats " + adjective1 + " " + food);
-       System.out.println("He normally " + verb1 + " after eating");
-       System.out.println("Ben always hangs out with " + name + " and they work together at " + job);
+    public static void main(final String[] args) throws Exception {
+        int count = 0;
+        String color = "red";
 
 
+
+       while (color) {
+           System.out.println("The count is " + count);
+           count = count + 1;
+       }
+
+       while (color == "red"){
+       System.out.println("The color is " + color);
+       color = "green";
+
+       }
+
+
+       System.out.println("count finished");
     }
 }
