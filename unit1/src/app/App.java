b98cbd820bed19db08ef6229b8da5072a3c412cf
AlexisDupont2 <816522@mystma.org>
2020-09-22 19:31:55 +0000

    madlibs input

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -8,19 +8,24 @@ public class App {
 
        System.out.println("Please enter a place.");
        String place = input.nextLine();
-       System.out.println("Ben regularly visits the " + place + " every tuesday.");
        System.out.println("Please enter an adjective ");
        String adjective1 = input.nextLine();
        System.out.println("Please enter a food (plural)");
        String food = input.nextLine();
-       System.out.println("While at " + place + " Ben eats " + adjective1 + " " + food);
        System.out.println("Please enter a verb");
        String verb1= input.nextLine();
-       System.out.println("He normally " + verb1 + " after eating");
        System.out.println("Please enter an adjective");
        String adjective2 = input.nextLine();
+       System.out.println("Please enter a name");
+       String name = input.nextLine();
+       System.out.println("Please enter a job");
+       String job = input.nextLine();
        System.out.println("he likes " + adjective2 + " friends");
-       
+       System.out.println("Ben regularly visits the " + place + " every tuesday.");
+       System.out.println("While at " + place + " Ben eats " + adjective1 + " " + food);
+       System.out.println("He normally " + verb1 + " after eating");
+       System.out.println("Ben always hangs out with " + name + " and they work together at " + job);
+
 
     }
 }
