6a21749a4824dbadf0e43783873c2c63ebb6cb5b
AlexisDupont2 <816522@mystma.org>
2020-09-24 17:01:02 +0000

    color number

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,23 +2,19 @@ package app;
 import java.util.Scanner;
 public class App {
     public static void main(final String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a color.");
+        String color = input.nextLine();
+
+        System.out.println("Please enter a number.");
+        int number = input.nextInt();
+
         int count = 0;
-        String color = "red";
 
-
-
-       while (color) {
-           System.out.println("The count is " + count);
-           count = count + 1;
-       }
-
-       while (color == "red"){
-       System.out.println("The color is " + color);
-       color = "green";
-
-       }
-
-
-       System.out.println("count finished");
+        while(count < number){
+            System.out.println(color);
+            count = count + 1;
+        }
     }
 }
