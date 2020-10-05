3e209033491a416b224a64431e7593b666288306
AlexisDupont2 <816522@mystma.org>
2020-10-05 19:34:24 +0000

    challenge 1 finished

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,39 +1,12 @@
 package app;
 import java.util.Scanner;
 public class App {
-    public static void main(final String[] args) {
+     public static void main(final String[] args) throws Exception{
         
-    
-    
-        final Scanner keyboard = new Scanner(System.in);
+     for (int i = 0; i <= 1000; i = i + 100){
+         System.out.println("The count is now " + i);
 
-        System.out.println("Pick a number 1-5 for random cat fact");{
-        final int value = keyboard.nextInt();
-
-            if (value == 1)
-             {
-                System.out.println(" They always land on their feet");
-             }
-             else if (value == 2)
-             {
-                System.out.println("A cat was the Mayor of an Alaskan town for 20 years");
-             }
-             else if (value == 3)
-             {
-                 System.out.println("Cats spend 70% of their lives sleeping");
-             }
-             else if (value == 4)
-             {
-                 System.out.println("Cats have 4 legs");
-             }
-             else if (value == 5)
-             {
-                 System.out.println("cats like drinking moving water");
-             }
-             else if (value >= 6)
-             {
-                 System.out.println("Error pick a number between 1-5");
-             }
-            }
+        }
+        System.out.println("finished");
    }
 }
