3d7827be396e21cb873272ad97d7f1169cc50df7
AlexisDupont2 <816522@mystma.org>
2020-10-06 19:51:09 +0000

    challenge 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,21 +1,29 @@
 package app;
 import java.util.Scanner;
+
 public class App {
      public static void main(final String[] args) throws Exception{
         
-     Scanner console = new Scanner(System.in);
+      Scanner insert = new Scanner(System.in);
     
      final int password = 11;
      
-     int attemps = 0;
-      final boolean athenticated = false;
-      do{
+     final int attemps = 0;
+      final boolean authenticated = false;
+     do {
          final int EnteredPassword;
-          System.out.println("Enter password");
-		EnteredPassword = console.nextInt();
+         System.out.println("Enter password");
+         EnteredPassword = insert.nextInt();
+         System.out.println("password entered is " + EnteredPassword);
         
+         if (password == EnteredPassword); {
+        System.out.println("the birds are working for the bourgeoisie");
+         boolean athenticated = true;
+         
+         } else {
+         System.out.println("Wrong password try again");
+         attemps ++;
     
-    }while(attemps < 3 && athenticated == false);
-            
+        }while(attemps < 3 && authenticated == false);{    
    }
 }
