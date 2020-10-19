ce0fc17591443437f7a27ef5aa5d68e2b3debb3a
AlexisDupont2 <816522@mystma.org>
2020-10-19 20:15:37 +0000

    Roulette Unfinished p1

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,23 +1,38 @@
 package app;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
      public static void main(final String[] args) throws Exception{
-         Scanner input = new Scanner(System.in);
+        
+        
+        
+        
+        Scanner input = new Scanner(System.in);
+         int chips = 100;
 
-         System.out.println("Please enter a number");
+         System.out.println("would you like to make an inside or outside bet?");
+         int inout1 = input.nextInt();
+         input.nextLine();
+
+         System.out.println("Enter the Number you would like to bet on");
          int num1 = input.nextInt();
          input.nextLine();
 
-         System.out.println("Please enter a higher number");
-         int num2 = input.nextInt();
+          System.out.println("How much would you like to bet?");
+         int bet1 = input.nextInt();
          input.nextLine();
 
-         for (int i = num1; i <= num2; i ++){
-             if (i % 2 == 0){
-                 System.out.println(i);
-             }
-         }
+         System.out.println("You will bet " + bet1 + " On " + num1);
+
+         Random rnd = new Random();
+         
+         // 0 to 36
+         int n1 = rnd.nextInt (37);
+         System.out.println("Sprinning.. ");
+         System.out.println(n1);
+         
+
 
     }
 
