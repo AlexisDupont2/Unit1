918cdf7fb6c71ef1f3a0b5b8c3ddcaec9e64964a
AlexisDupont2 <816522@mystma.org>
2020-11-02 21:15:59 +0000

    Roulettte pt 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,91 +1,99 @@
 package app;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-     public static void main(final String[] args) throws Exception{
-         String redblk[]={"0","RED","BLACK","RED","BLACK","RED", "BLACK", "RED", "BLACK","RED","BLACK", "BLACK","RED",
-         "BLACK","RED","BLACK","RED","BLACK","RED", "RED","BLACK","RED","BLACK","RED","BLACK", "RED",
-         "BLACK", "RED", "BLACK", "BLACK","RED","BLACK","RED", "BLACK","RED", "BLACK", "RED"};
-         int rbnum[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
-         String evenodd[]={"even","odd","even","odd","even","odd", "even", "odd","even", "odd", "even","odd","even","odd","even","odd","even","odd", "even", "odd","even", "odd",
-          "even","odd","even","odd","even","odd","even","odd", "even", "odd","even", "odd", "even","odd",};
-        
+
+    public static void main(final String[] args) throws Exception {
+        String redblk[] = { "0", "RED", "BLACK", "RED", "BLACK", "RED", "BLACK", "RED", "BLACK", "RED", "BLACK",
+                "BLACK", "RED", "BLACK", "RED", "BLACK", "RED", "BLACK", "RED", "RED", "BLACK", "RED", "BLACK", "RED",
+                "BLACK", "RED", "BLACK", "RED", "BLACK", "BLACK", "RED", "BLACK", "RED", "BLACK", "RED", "BLACK",
+                "RED" };
+        int rbnum[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+        String evenodd[] = { "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd",
+                "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd",
+                "even", "odd", "even", "odd", "even", "odd", "even", "odd", "even", "odd", };
+
         int num1 = 0;
         boolean keepgo = false;
-        boolean  keepgo1 = false;
+        boolean keepgo1 = false;
         Scanner input = new Scanner(System.in);
-         int chips = 100;
+        String outsidebet = null;
+        int chips = 100;
 
-         while (keepgo1 == false){
-        while (keepgo == false){
-         System.out.println("would you like to make an inside or outside bet? (enter in lowercase)");
-         String inout1 = input.nextLine();
+        while (keepgo1 == false) {
+            while (keepgo == false) {
+                System.out.println("would you like to make an inside or outside bet? (enter in lowercase)");
+                String inout1 = input.nextLine();
 
-         if (inout1.equals("inside") ) {
-             System.out.println("Enter the Number you would like to bet on");
-             num1 = input.nextInt();
-             input.nextLine();
-         }
-         if ( inout1.equals("outside") ) {
-             System.out.println("Would you like to bet even, odd, black, or red?");
-             String outsidebet = input.nextLine();
-             input.nextLine();
-         }
+                if (inout1.equals("inside")) {
+                    System.out.println("Enter the Number you would like to bet on");
+                    num1 = input.nextInt();
+                    input.nextLine();
+                }
+                if (inout1.equals("outside")) {
+                    System.out.println("Would you like to bet even, odd, black, or red?");
+                    outsidebet = input.nextLine();
+                }
 
-          System.out.println("How much would you like to bet?");
-         int bet1 = input.nextInt();
-         input.nextLine();
+                System.out.println("How much would you like to bet?");
+                int bet1 = input.nextInt();
+                input.nextLine(); 
+                    System.out.println("You will bet $" + bet1 + " On " + num1);
+                    input.nextLine();
 
-         System.out.println("You will bet $" + bet1 + " On " + num1);
-         input.nextLine();
+                System.out.println("would you like to bet again? you have " + chips + " chips remaining");
+                String kepgo = input.nextLine();
+                if (kepgo.equals("yes")) {
+                    keepgo = false;
+                }
+                if (kepgo.equals("no")) {
+                    keepgo = true;
+                }
+            }
 
-         System.out.println("would you like to bet again? you have " + chips + " chips remaining");
-         String kepgo =input.nextLine();
-         if(kepgo == "yes"){
-        keepgo = false;
-         }
-         if(kepgo == "no"){
-         keepgo = true;
-         }
+            Random rnd = new Random();
+
+            // 0 to 36
+            int n1 = rnd.nextInt(37);
+            String resultcolor;
+            System.out.println("Sprinning.. ");
+
+            if (redblk[n1].equals("BLACK")) {
+                resultcolor = "BLACK";
+            } else if (n1 == 0) {
+                resultcolor = "green";
+            } else {
+                resultcolor = "red";
+            }
+
+            System.out.println(rbnum[n1]);
+            System.out.println(redblk[n1]);
+            System.out.println(evenodd[n1]);
+            input.nextLine();
+            if (rbnum[n1] == num1) {
+                System.out.println("Winner!");
+            }
+            if (redblk[n1].equals(outsidebet) ) {
+            System.out.println("Winner!");
         }
-        
-         Random rnd = new Random();
-         
-         // 0 to 36
-         int n1 = rnd.nextInt (37);
-         String resultcolor;
-         System.out.println("Sprinning.. ");
-         
-         if (redblk[n1].equals("BLACK")){
-             resultcolor= "BLACK";
-         }
-         else if(n1==0){
-             resultcolor="green";
-         }
-         else{
-             resultcolor="red";
-         }
-         
-         System.out.println(rbnum[n1]); System.out.println(redblk[n1]);
-        input.nextLine();
-        
-        
-        System.out.println("Would you like to keep playing?");
-        String kepgo1 = input.nextLine();
-        if (kepgo1 == "yes"){
-            keepgo1 = false;
+        else {
+            System.out.println(" You lost, You now have " + chips + "chips remaining");
         }
-        else if(kepgo1 =="no"){
-            
-            keepgo1 = true;
-        }
-    System.out.println("Please close program");
-        
-    
 
-        
+            System.out.println("Would you like to keep playing?");
+            String kepgo1 = input.nextLine();
+            if (kepgo1.equals("yes")) {
+                keepgo1 = false;
+            }
+            if (kepgo1.equals("no")) {
+
+                keepgo1 = true;
+            }
+            System.out.println("Please close program");
+
+        }
     }
-
-   }
 }
